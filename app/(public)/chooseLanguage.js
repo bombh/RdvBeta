9db91c9c57c436af3f3f5830c989541998fb03c7
bombh <fabrice@bxxl.be>
2023-12-21 17:01:31 +0100

    delete styled()

--- app/(public)/chooseLanguage.js
+++ app/(public)/chooseLanguage.js
@@ -1,45 +1,41 @@
 import { View, Text, Pressable } from 'react-native'
 import { Stack } from 'expo-router/stack'
 import { useRouter } from "expo-router"
-import { styled } from 'nativewind'
 
-const StyledView = styled(View);
-const StyledText = styled(Text);
-const StyledPressable = styled(Pressable);
-
-const router = useRouter()
 
 
 const ChooseLanguage = () => {
-  return (
+   const router = useRouter()
+   
+   return (
    <>
       <Stack.Screen options={{
          headerTitle: 'Rendez-Vous',
          headerTitleAlign: 'center',
       }} />
       
-      <StyledView className="flex-1 items-center justify-center bg-white">
-         <StyledText className="text-gray-500 text-4xl mb-2">Home</StyledText>
-         <StyledText className="text-black text-sm mb-6">Select your language</StyledText>
+      <View className="flex-1 items-center justify-center bg-white">
+         <Text className="text-gray-500 text-4xl mb-2">Home</Text>
+         <Text className="text-black text-sm mb-6">Select your language</Text>
 
          
-         <StyledView className="flex-row w-full justify-center mb-3">
-            <StyledPressable
+         <View className="flex-row w-full justify-center mb-3">
+            <Pressable
                className="w-3/5 bg-sky-500 p-2 active:bg-sky-600 rounded-md"
                onPress={ () => router.push(`/subscribe`)}>
-               <StyledText className="text-lg text-white text-center">Français</StyledText>
-            </StyledPressable>
-         </StyledView>
+               <Text className="text-lg text-white text-center">Français</Text>
+            </Pressable>
+         </View>
 
-         <StyledView className="flex-row w-full justify-center">
-            <StyledPressable
+         <View className="flex-row w-full justify-center">
+            <Pressable
                className="w-3/5 bg-sky-500 p-2 active:bg-sky-600 rounded-md"
                onPress={ () => router.push(`/subscribe`)}>
-                  <StyledText className="text-lg fonsem text-white text-center">Nederlands</StyledText>
-            </StyledPressable>
-         </StyledView>
+                  <Text className="text-lg fonsem text-white text-center">Nederlands</Text>
+            </Pressable>
+         </View>
          
-      </StyledView>
+      </View>
    </>
   )
 }
