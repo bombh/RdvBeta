445cad701c9f791aadfcbc112a3d55912954774f
bombh <fabrice@bxxl.be>
2024-01-18 22:54:38 +0100

    Colors

--- src/components/profile/common/ButtonPicture.js
+++ src/components/profile/common/ButtonPicture.js
@@ -5,15 +5,16 @@ import { CameraIcon } from 'react-native-heroicons/solid'
 const ButtonPicture = ( {onPress }) => {
 
    return (
-      <Pressable onPress={onPress} className="bg-white rounded-full w-12 h-12 border border-neutral-300 flex items-center justify-center active:bg-neutral-900 active:border-neutral-900">
+      <View className="bg-white rounded-full w-12 h-12 border border-neutral-300 flex items-center justify-center">
+         
          <CameraIcon size={30} color={colors.neutral[400]} />
 
-         {/* Online status */}
+         {/* Num picture */}
          <View className="flex items-center justify-center rounded-full bg-amber-500 h-5 absolute -top-1 left-8 px-2">
             <Text className='text-white'>4</Text>
          </View>
 
-      </Pressable>
+      </View>
    )
 }
 
